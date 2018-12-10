eccf1d84c50a9d5325dbc3a6edee56ec6828fea0
ParksAaron <aaparks@csumb.edu>
2018-12-09 22:52:55 -0800

    added question form that posts to stream page

--- musicRecommendation.py
+++ musicRecommendation.py
@@ -6,6 +6,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_dance.contrib.spotify import make_spotify_blueprint, spotify
 from flask_bootstrap import Bootstrap
+from oauthlib.oauth2.rfc6749.errors import InvalidGrantError, TokenExpiredError, OAuth2Error
 import requests, json, urllib.parse
 
 app = Flask(__name__)
@@ -18,12 +19,23 @@ blueprint = make_spotify_blueprint(
 	scope='playlist-modify-public streaming user-library-read',
 )
 
+questions = ["I am a very sociable person.", "I like to party.", "I am energetic.", "I enjoy dancing.", "I enjoy happy songs.", "I enojoy instrumental songs." ]
+
 app.register_blueprint(blueprint, url_prefix='/login')
 
 @app.route('/')
 def index():
     if not spotify.authorized:
         return redirect(url_for('spotify.login'))
-    search_string = urllib.parse.quote('The Birthday Party')
-    resp = spotify.get(f'v1/search?q={search_string}&type=artist')
-    return render_template('musicRecommendation_home.html', data=resp.json())
+    try: 
+    	search_string = urllib.parse.quote('The Birthday Party')
+    	resp = spotify.get(f'v1/search?q={search_string}&type=artist')
+    	return render_template('musicRecommendation_home.html', questions=questions)
+    except(InvalidGrantError, TokenExpiredError) as e:
+    	return redirect(url_for('spotify.login'))
+
+@app.route('/stream', methods = ['POST', 'GET'])
+def stream():
+	if request.method == 'POST':
+		answers = request.form
+		return render_template('musicRecommendation_stream.html', answers=answers)
